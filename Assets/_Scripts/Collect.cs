362d9180966807c6d3503519037a80ddf9479a29
e-cooper <elicooper18@gmail.com>
2015-03-29 16:39:27 -0400

    initial commit
    Basic player interaction with scene

---
+++ Assets/_Scripts/Collect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
